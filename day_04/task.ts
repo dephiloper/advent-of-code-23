62cb9f8c0aef7dcb8ec5ed997c3b9164a3d19691
Philipp Bönsch <philipp.boensch@softgames.de>
2023-12-14 19:57:59 +0100

    AoC d4-2

--- day_04/task.ts
+++ day_04/task.ts
@@ -2,10 +2,15 @@ import fs from 'fs';
 const filePath = './day_04/cards.txt';
 
 class Card {
+    index: number = -1;
     winningNumbers: number[] = [];
     drawnNumbers: number[] = [];
     matchingNumbers: number[] = [];
     score: number = 0;
+
+    public toString(): string {
+        return `index ${this.index}`
+    }
 }
 
 export function day04() {
@@ -20,11 +25,12 @@ export function day04() {
 
         // format 'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
 
-        for (const line of lines) {
+        for (let i = 0; i < lines.length; i++) {
             const card = new Card();
+            card.index = i + 1;
 
             // remove Card index
-            const rawCards = line.split(': ')[1]
+            const rawCards = lines[i].split(': ')[1]
                 // replace all multiple spaces with one space
                 .replace(/\s\s+/g, ' ')
                 // separate the two parts
@@ -44,8 +50,33 @@ export function day04() {
             cards.push(card);
         }
 
-        // console.log(cards);
         console.log('How many points are they worth in total?', cards.reduce((acc, card) => acc + card.score, 0));
+
+        const wonScratchCards = [...cards];
+
+        for (let i = 0; i < wonScratchCards.length; i++) {
+            const matchingNumbers = wonScratchCards[i].matchingNumbers.length;
+            const index = cards.indexOf(wonScratchCards[i]);
+
+            Array.from({ length: matchingNumbers }).forEach((_, j) => {
+                wonScratchCards.push(cards[index + j + 1]);
+            });
+
+        }
+
+        const countMap = new Map<string, number>();
+
+        wonScratchCards.forEach((card) => {
+            const index = `index ${card.index}`;
+            if (countMap.has(index)) {
+                countMap.set(index, countMap.get(index)! + 1);
+            } else {
+                countMap.set(index, 1);
+            }
+        });
+
+        console.log('how many total scratchcards do you end up with?', wonScratchCards.length);
+        console.log(countMap);
     });
 
 }
