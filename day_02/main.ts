c7fc1157d76341a6f883bf6f3498587c0ed1003a
Philipp Bönsch <philipp.boensch@softgames.de>
2023-12-14 16:17:05 +0100

    AoC d2-2

--- day_02/main.ts
+++ day_02/main.ts
@@ -2,6 +2,9 @@ import fs from 'fs';
 
 class Game {
     sets: GameSet[] = [];
+    maxGreen: number = 0;
+    maxBlue: number = 0;
+    maxRed: number = 0;
 }
 
 class GameSet {
@@ -39,31 +42,39 @@ fs.readFile(filePath, 'utf8', (err, data) => {
             for (const element of elements) {
                 if (element.includes('green')) {
                     set.green = parseInt(element.split(' ')[0]);
+                    game.maxGreen = set.green > game.maxGreen ? set.green : game.maxGreen;
                     set.valid = set.green > MaxValues.GREEN ? false : set.valid;
                 }
                 if (element.includes('blue')) {
                     set.blue = parseInt(element.split(' ')[0]);
+                    game.maxBlue = set.blue > game.maxBlue ? set.blue : game.maxBlue;
                     set.valid = set.blue > MaxValues.BLUE ? false : set.valid;
 
                 }
                 if (element.includes('red')) {
                     set.red = parseInt(element.split(' ')[0]);
+                    game.maxRed = set.red > game.maxRed ? set.red : game.maxRed;
                     set.valid = set.red > MaxValues.RED ? false : set.valid;
                 }
             }
 
             game.sets.push(set);
         }
-        
+
         games.push(game);
     }
 
-    let sum = 0;
+    let validSetSum = 0;
+    let powerOfSetSum = 0;
+
     games.forEach((game, idx) => {
         if (game.sets.every(set => set.valid)) {
-            sum += idx + 1;
+            validSetSum += idx + 1;
         }
+
+        powerOfSetSum += game.maxBlue * game.maxGreen * game.maxRed;
     });
 
-    console.log(sum);
+    console.log('What is the sum of the IDs of those games?', validSetSum);
+    console.log('What is the sum of the power of these sets?', powerOfSetSum);
 });
