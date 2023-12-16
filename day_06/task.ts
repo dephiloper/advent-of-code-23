43be3310403d355cef5e06950929ae8738c69f2e
Philipp Bönsch <philipp.boensch@softgames.de>
2023-12-16 16:40:15 +0100

    AoC d6-2

--- day_06/task.ts
+++ day_06/task.ts
@@ -6,44 +6,34 @@ export function day06() {
     const data = fs.readFileSync(filePath, 'utf8');
     const lines = data.split('\n');
 
-    const times: number[] = [];
-    const distances: number[] = [];
+    let time: number = -1;
+    let distance: number = -1;
 
     lines.forEach((line, index) => {
-        // replace multiple spaces and tabs with a single space
-        line = line.replace(/\s\s+/g, ' ');
+        // remove all spaces and tabs
+        line = line.replace(/\s\s+/g, '');
 
-        line = line.split(': ')[1];
+        line = line.split(':')[1];
 
         if (index === 0) {
-            times.push(...line.split(' ').map(Number));
+            time = Number(line);
         } else {
-            distances.push(...line.split(' ').map(Number));
+            distance = Number(line);
         }
     });
 
-    let betterTimesPerRace: number[] = [];
 
-    for (let i = 0; i < times.length; i++) {
-        const time = times[i];
-        const distance = distances[i];
-        betterTimesPerRace.push(0);
-        for (let j = 0; j <= time; j++) {
-            const buttonHold = j; // aka boat speed mm/ms
-            const timeLeft = time - j;
-            const distanceTravelled = buttonHold * timeLeft; // in mm
-            if (distanceTravelled > distance) {
-                // console.log(`race: ${i + 1}, hold down: ${buttonHold}ms, time left: ${timeLeft}ms, distace: ${distanceTravelled}mm`);
-                betterTimesPerRace[i]++;
-            }
+    let betterTimesPerRace: number = 0;
+
+    for (let j = 0; j <= time; j++) {
+        const buttonHold = j; // aka boat speed mm/ms
+        const timeLeft = time - j;
+        const distanceTravelled = buttonHold * timeLeft; // in mm
+        if (distanceTravelled > distance) {
+            // console.log(`race: ${i + 1}, hold down: ${buttonHold}ms, time left: ${timeLeft}ms, distace: ${distanceTravelled}mm`);
+            betterTimesPerRace++;
         }
     }
 
-    // multiply all the better times together
-    let betterTimesMultiplied = 1;
-    betterTimesPerRace.forEach((betterTime) => {
-        betterTimesMultiplied *= betterTime;
-    });
-
-    console.log('What do you get if you multiply these numbers together?', betterTimesMultiplied);
+    console.log('How many ways can you beat the record in this one much longer race?', betterTimesPerRace);
 }
